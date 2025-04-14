c14df068341b0b5bd850913b3a6a5d498f19faaa
Maxence Cholet <153302107+Toblek@users.noreply.github.com>
2025-04-14 10:54:20 +0200

    Revert "Merge branch 'karell'"
    This reverts commit 30b0b1d0ce322e761e969eb651888b223887e0ab, reversing
    changes made to 82a28a94a831e66d2d1551152931d2921157a192.

--- Assets/Script/ScriptableScript/Quest.cs
+++ Assets/Script/ScriptableScript/Quest.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Quest", menuName = "Scriptable/Quest", order = 1)]
@@ -8,8 +7,7 @@ public class Quest : ScriptableObject
 {
     public TypeOfQuest type;
     public int questID;
-    [SerializeField] public string questName;
-    [SerializeField] public Quest nextQuest; 
+    [SerializeField] public string Name;
     [SerializeField] public int CurrentStep;
     [SerializeField] public StepQuest[] Step;
     [SerializeField] public string[] Description;
@@ -23,33 +21,6 @@ public class Quest : ScriptableObject
         Talk,
         Pick,
         Other,
-    }
-
-    public void ResetQuest()
-    {
-        Finish = new bool[Step.Length];
-        CurrentStep = 0;
-    }
-
-    public bool IsCompleted()
-    {
-        foreach (bool step in Finish)
-        {
-            if (!step) return false;
-        }
-        return true;
-    }
-
-    public void MarkStepAsDone(string targetName)
-    {
-        if (Step != null && CurrentStep < Step.Length)
-        {
-            if (Step[CurrentStep].NameOfTarget == targetName)
-            {
-                Finish[CurrentStep] = true;
-                CurrentStep++;
-            }
-        }
     }
 }
 
